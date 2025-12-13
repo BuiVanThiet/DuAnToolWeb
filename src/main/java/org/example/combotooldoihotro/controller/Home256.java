d8cf09e905e49c8214d8aa2e7adf696a937eb94a
BuiVanThiet <thietbuivan2512@gmail.com>
2025-12-13 10:58:06 +0700

    ngay131220251058

--- src/main/java/org/example/combotooldoihotro/controller/Home256.java
+++ src/main/java/org/example/combotooldoihotro/controller/Home256.java
@@ -9,8 +9,24 @@ public class Home256 {
     public String getHomeTool() {
         return "homeTool";
     }
+    @GetMapping("/list-product-company")
+    public String getHomeDanhSachSanPham() {
+        return "HomeDanhSachSanPham";
+    }
     @GetMapping("/dowload-image")
     public String getHomeDowloadImage() {
         return "HomeDowloadImage";
+    }
+    @GetMapping("/team-support-tool")
+    public String getHomeTeamSupport() {
+        return "HomeTeamHoTro";
+    }
+    @GetMapping("/report-and-update")
+    public String getHomeBaoCaoVaUpdate() {
+        return "HomeBaoCaoVaUpdate";
+    }
+    @GetMapping("/setting")
+    public String getHomeSetting() {
+        return "HomeCaiDat";
     }
 }
